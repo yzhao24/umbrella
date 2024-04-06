3e068490eaae657306097b4ca675bdbec8d4d202
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-04-06 23:02:42 +0000

    first attempt

---
+++ umbrella.rb
@@ -0,0 +1,20 @@
+require "http"
+
+# Hidden variables
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_API_KEY")
+
+# Assemble the full URL string by adding the first part, the API token, and the last part together
+pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+
+# Place a GET request to the URL
+raw_response = HTTP.get(pirate_weather_url)
+
+require "json"
+
+parsed_response = JSON.parse(raw_response)
+
+currently_hash = parsed_response.fetch("currently")
+
+current_temp = currently_hash.fetch("temperature")
+
+puts "The current temperature is " + current_temp.to_s + "."
