0d77f857dfb6647f2df922518b5e59389b7a99bb
yzhao24 <165528874+yzhao24@users.noreply.github.com>
2024-04-07 00:32:47 +0000

    update the rest

--- umbrella.rb
+++ umbrella.rb
@@ -4,13 +4,12 @@ pp "Where are you located?"
 
 user_location = gets.chomp.gsub(" ", "%20")
 
-# user_location  = "Chicago"
+# user_location  = "Tampa"
 
 pp user_location
 
 maps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=" + user_location + "&key=" + ENV.fetch("GMAPS_KEY")
 
-require "http"
 
 resp = HTTP.get (maps_url)
 
@@ -34,21 +33,59 @@ pp longitude = loc.fetch("lng")
 
 
 
-# # Hidden variables
-# pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_KEY")
+# Hidden variables
+pirate_weather_api_key = ENV.fetch("PIRATE_WEATHER_API_KEY")
 
-# # Assemble the full URL string by adding the first part, the API token, and the last part together
-# pirate_weather_url = "https://api.pirateweather.net/forecast/" + pirate_weather_api_key + "/41.8887,-87.6355"
+# Assemble the full URL string by adding the first part, the API token, and the last part together
+pirate_weather_url = "https://api.pirateweather.net/forecast/#{pirate_weather_api_key}/#{latitude},#{longitude}"
 
-# # Place a GET request to the URL
-# raw_response = HTTP.get(pirate_weather_url)
+raw_pirate_weather_data = HTTP.get(pirate_weather_url)
 
-# require "json"
+parsed_pirate_weather_data = JSON.parse(raw_pirate_weather_data)
 
-# parsed_response = JSON.parse(raw_response)
+currently_hash = parsed_pirate_weather_data.fetch("currently")
 
-# currently_hash = parsed_response.fetch("currently")
+current_temp = currently_hash.fetch("temperature")
 
-# current_temp = currently_hash.fetch("temperature")
+pp "It is currently #{current_temp}°F."
 
-# puts "The current temperature is " + current_temp.to_s + "."
+# Some locations around the world do not come with minutely data.
+minutely_hash = parsed_pirate_weather_data.fetch("minutely", false)
+
+if minutely_hash
+  next_hour_summary = minutely_hash.fetch("summary")
+
+  pp "Next hour: #{next_hour_summary}"
+end
+
+hourly_hash = parsed_pirate_weather_data.fetch("hourly")
+
+hourly_data_array = hourly_hash.fetch("data")
+
+next_twelve_hours = hourly_data_array[1..12]
+
+precip_prob_threshold = 0.10
+
+any_precipitation = false
+
+next_twelve_hours.each do |hour_hash|
+  precip_prob = hour_hash.fetch("precipProbability")
+
+  if precip_prob > precip_prob_threshold
+    any_precipitation = true
+
+    precip_time = Time.at(hour_hash.fetch("time"))
+
+    seconds_from_now = precip_time - Time.now
+
+    hours_from_now = seconds_from_now / 60 / 60
+
+    pp "In #{hours_from_now.round} hours, there is a #{(precip_prob * 100).round}% chance of precipitation."
+  end
+end
+
+if any_precipitation == true
+  pp "You might want to take an umbrella!"
+else
+  pp "You probably won't need an umbrella."
+end
